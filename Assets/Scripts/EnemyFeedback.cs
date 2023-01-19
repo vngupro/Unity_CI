47dc328b5758064aa6b2fa73b48bdb2c6c7ed8b5
Elvis <elvishenry2402@gmail.com>
2023-01-19 23:15:38 +0100

    Add Focus Feedback

---
+++ Assets/Scripts/EnemyFeedback.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFeedback : MonoBehaviour
+{
+    private bool isFocused = false;
+    
+    private Renderer rend;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SetFocus(bool focus)
+    {
+        isFocused = focus;
+        rend.material.color = isFocused ? Color.red : Color.white;
+    }
+}
