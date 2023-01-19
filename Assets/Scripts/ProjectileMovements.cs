70caf74e5660f4fcd88e119ecb632bbf6f4f45d8
Lych <Aqu4men@gmail.com>
2023-01-19 22:24:42 +0100

    Player attack and projectile
    waiting to link with PlayerMovement script

---
+++ Assets/Scripts/ProjectileMovements.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovements : MonoBehaviour
+{
+    public int bouncingInt;
+    private Rigidbody rb;
+    private Vector3 velocity_Last_Frame;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    private void LateUpdate()
+    {
+        velocity_Last_Frame = rb.velocity;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(bouncingInt > 0)
+        {
+            Vector3 normal = collision.GetContact(0).normal;
+            Vector3 dir = Vector3.Reflect(velocity_Last_Frame, normal);
+            rb.velocity = dir;
+            Debug.DrawLine(transform.position, transform.position + (dir * 5),Color.red,999999);
+            bouncingInt--;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
